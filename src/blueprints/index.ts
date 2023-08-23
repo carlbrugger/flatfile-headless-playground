03404323014d1f1cddbe9b91bd1f4d7033139cb1
Carl Brugger <carl@simpleupdates.com>
2023-08-23 08:55:26 -0500

    Add sheet blueprints

---
+++ src/blueprints/index.ts
@@ -0,0 +1,2 @@
+export * from './sheets/getting_started'
+export * from './sheets/field_types'
---
+++ src/blueprints/sheets/field_types.ts
@@ -0,0 +1,50 @@
+import { Flatfile } from '@flatfile/api'
+
+export const fieldTypesSheet: Flatfile.SheetConfig = {
+  name: 'Field Types',
+  slug: 'field_types',
+  fields: [
+    {
+      key: 'string',
+      type: 'string',
+      label: 'String',
+    },
+    {
+      key: 'number',
+      type: 'number',
+      label: 'Number',
+    },
+    {
+      key: 'date',
+      type: 'date',
+      label: 'Date',
+    },
+    {
+      key: 'boolean',
+      type: 'boolean',
+      label: 'Boolean',
+    },
+    {
+      key: 'enum',
+      type: 'enum',
+      label: 'Enum',
+      config: {
+        options: [
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2' },
+          { label: 'Option 3', value: 'option3' },
+        ],
+      },
+    },
+    {
+      key: 'reference',
+      type: 'reference',
+      label: 'Reference',
+      config: {
+        key: 'email',
+        ref: 'contacts',
+        relationship: 'has-one',
+      },
+    },
+  ],
+}
