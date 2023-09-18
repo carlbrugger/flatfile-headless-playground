0e113ee60d8acd33c85c8b729a937498010e2f2b
Carl Brugger <carl@simpleupdates.com>
2023-09-18 17:05:55 -0500

    Excel test

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,90 @@
-import api from '@flatfile/api'
+import { Flatfile } from '@flatfile/api'
 import type { FlatfileListener } from '@flatfile/listener'
-
-import { XMLExtractor } from '@flatfile/plugin-xml-extractor'
-import { PSVExtractor } from '@flatfile/plugin-psv-extractor'
-import { JSONExtractor } from '@flatfile/plugin-json-extractor'
+import { configureSpace } from '@flatfile/plugin-space-configure'
 import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor'
-import { ZipExtractor } from '@flatfile/plugin-zip-extractor'
-import { TSVExtractor } from '@flatfile/plugin-tsv-extractor'
-import { DelimiterExtractor } from '@flatfile/plugin-delimiter-extractor'
-import { dedupePlugin } from '@flatfile/plugin-dedupe'
-
-import { contactsSheet } from './blueprints'
 
 export default async function (listener: FlatfileListener) {
-  listener.on('**', (event) => {
-    console.log(event.topic)
-  })
-
-  // Extractors
-  listener.use(JSONExtractor())
-  listener.use(ExcelExtractor())
-  listener.use(XMLExtractor())
-  listener.use(PSVExtractor())
-  // listener.use(DelimiterExtractor('psv', { delimiter: '|' }))
-  listener.use(TSVExtractor())
-  // listener.use(DelimiterExtractor('tsv', { delimiter: '\t' }))
-  listener.use(DelimiterExtractor('txt', { delimiter: '~' }))
+  listener.use(ExcelExtractor({ chunkSize: 30000, parallel: 3 }))
   listener.use(
-    DelimiterExtractor('semicolin', {
-      delimiter: ';',
-      transform: (v) => v.toUpperCase(), // <-- transform is a custom function run during extraction
+    configureSpace({
+      workbooks: [
+        {
+          name: 'Workbook One',
+          sheets: [sheet],
+          actions: [
+            {
+              operation: 'submitActionFg',
+              mode: 'foreground',
+              label: 'Submit data',
+              type: 'string',
+              description: 'Submit this data to a webhook.',
+              primary: true,
+            },
+          ],
+        },
+      ],
     })
   )
-  listener.use(ZipExtractor())
+}
 
-  listener.use(
-    dedupePlugin('dedupeEmail', {
-      on: 'email',
-      keep: 'last',
-    })
-  )
-
-  listener.filter({ job: 'space:configure' }, (configure) => {
-    configure.on(
-      'job:ready',
-      async ({ context: { spaceId, environmentId, jobId } }) => {
-        await api.jobs.ack(jobId, {
-          info: 'Configuring space...',
-          progress: 10,
-        })
-
-        await api.workbooks.create({
-          spaceId,
-          environmentId,
-          name: 'All Data',
-          labels: ['pinned'],
-          sheets: [contactsSheet],
-        })
-
-        await api.jobs.complete(jobId, {
-          outcome: {
-            message: 'Space configured successfully!',
-          },
-        })
-      }
-    )
-  })
+export const sheet: Flatfile.SheetConfig = {
+  name: 'Sheet',
+  slug: 'sheet',
+  fields: [
+    {
+      key: 'serial',
+      type: 'string',
+      label: 'Serial',
+    },
+    {
+      key: 'productName',
+      type: 'string',
+      label: 'Product Name',
+    },
+    {
+      key: 'office',
+      type: 'string',
+      label: 'Office',
+    },
+    {
+      key: 'creation',
+      type: 'string',
+      label: 'Creation',
+    },
+    {
+      key: 'department',
+      type: 'string',
+      label: 'Department',
+    },
+    {
+      key: 'color',
+      type: 'string',
+      label: 'Color',
+    },
+    {
+      key: 'size',
+      type: 'string',
+      label: 'Size',
+    },
+    {
+      key: 'quality',
+      type: 'string',
+      label: 'Quality',
+    },
+    {
+      key: 'packSize',
+      type: 'string',
+      label: 'Pack Size',
+    },
+    {
+      key: 'msrpp',
+      type: 'string',
+      label: 'MSRPP',
+    },
+    {
+      key: 'vat',
+      type: 'string',
+      label: 'VAT',
+    },
+  ],
 }
