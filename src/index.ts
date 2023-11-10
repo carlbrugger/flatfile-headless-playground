e845e6df87b2b397fa53950af4ec92b518e2b9aa
Carl Brugger <carl.brugger@flatfile.io>
2023-11-09 19:42:34 -0600

    Automatically download an extracted file as a csv

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,56 @@
+import api from '@flatfile/api'
 import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
-import { FlatfileRecord, recordHook } from '@flatfile/plugin-record-hook'
+import { JSONExtractor } from '@flatfile/plugin-json-extractor'
+import { configureSpace } from '@flatfile/plugin-space-configure'
 
 export default async function (listener: FlatfileListener) {
+  listener.use(JSONExtractor())
+  listener.on(
+    'job:ready',
+    { operation: 'extract*' },
+    async (event: FlatfileEvent) => {
+      const { fileId } = event.context
+      const { data: file } = await api.files.get(fileId)
+      const { data: workbook } = await api.workbooks.get(file.workbookId)
+      const sheetId = workbook.sheets[0].id
+
+      try {
+        await api.jobs.create({
+          type: 'sheet',
+          operation: 'export',
+          trigger: 'immediate',
+          source: sheetId,
+          config: {
+            options: {
+              filter: 'all',
+            },
+          },
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  )
   listener.use(
-    recordHook('contacts', (r: FlatfileRecord, event: FlatfileEvent) => {
-      const email = r.get('email') as string
-      if (!email) {
-        console.log('Email is required')
-        r.addError('email', 'Email is required')
-      }
-
-      const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-      if (email !== null && !validEmailAddress.test(email)) {
-        console.log('Invalid email address')
-        r.addError('email', 'Invalid email address')
-      }
-
-      return r
+    configureSpace({
+      workbooks: [
+        {
+          name: 'Workbook',
+          sheets: [
+            {
+              name: 'Sheet',
+              slug: 'sheet',
+              fields: [
+                {
+                  key: 'name',
+                  type: 'string',
+                  label: 'Name',
+                },
+              ],
+            },
+          ],
+        },
+      ],
     })
   )
 }
