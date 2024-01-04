795745f1b0a3f60bfb5bee6fc087b276ff44d835
Carl Brugger <carl.brugger@flatfile.io>
2024-01-04 12:38:52 -0600

    Partial rejections demo

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,66 @@
-import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
-import { FlatfileRecord, recordHook } from '@flatfile/plugin-record-hook'
+import type { FlatfileListener } from '@flatfile/listener'
+import { configureSpace } from '@flatfile/plugin-space-configure'
+import { webhookEgress } from '@flatfile/plugin-webhook-egress'
 
-export default async function (listener: FlatfileListener) {
+export default function (listener: FlatfileListener) {
   listener.use(
-    recordHook('contacts', (r: FlatfileRecord, event: FlatfileEvent) => {
-      const email = r.get('email') as string
-      if (!email) {
-        console.log('Email is required')
-        r.addError('email', 'Email is required')
-      }
+    webhookEgress(
+      'workbook:submitActionFg',
+      'http://localhost:5678/reject-non-flatfile-emails'
+    )
+  )
 
-      const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-      if (email !== null && !validEmailAddress.test(email)) {
-        console.log('Invalid email address')
-        r.addError('email', 'Invalid email address')
-      }
-
-      return r
+  listener.namespace(
+    ['space:getting-started'],
+    configureSpace({
+      workbooks: [
+        {
+          name: 'Playground',
+          sheets: [
+            {
+              name: 'Contacts',
+              slug: 'contacts',
+              fields: [
+                {
+                  key: 'firstName',
+                  type: 'string',
+                  label: 'First Name',
+                  constraints: [{ type: 'required' }, { type: 'unique' }],
+                },
+                {
+                  key: 'lastName',
+                  type: 'string',
+                  label: 'Last Name',
+                },
+                {
+                  key: 'email',
+                  type: 'string',
+                  label: 'Email',
+                },
+              ],
+            },
+          ],
+          // settings: {
+          //   trackChanges: true,
+          // },
+          actions: [
+            {
+              operation: 'submitActionFg',
+              mode: 'foreground',
+              label: 'Submit data',
+              type: 'string',
+              description: 'Submit this data to a webhook.',
+              primary: true,
+            },
+            {
+              operation: 'downloadWorkbook',
+              mode: 'foreground',
+              label: 'Download Workbook',
+              description: 'Downloads Excel Workbook of Data',
+            },
+          ],
+        },
+      ],
     })
   )
 }
