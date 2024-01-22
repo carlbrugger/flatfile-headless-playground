431a506cf0497dcc7c1daed843eaab3679906d1e
Carl Brugger <carl.brugger@flatfile.io>
2024-01-22 15:53:44 -0600

    extractor demo

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
-import { FlatfileRecord, recordHook } from '@flatfile/plugin-record-hook'
+import type { FlatfileListener } from '@flatfile/listener'
+import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor'
 
-export default async function (listener: FlatfileListener) {
-  listener.use(
-    recordHook('contacts', (r: FlatfileRecord, event: FlatfileEvent) => {
-      const email = r.get('email') as string
-      if (!email) {
-        console.log('Email is required')
-        r.addError('email', 'Email is required')
-      }
-
-      const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-      if (email !== null && !validEmailAddress.test(email)) {
-        console.log('Invalid email address')
-        r.addError('email', 'Invalid email address')
-      }
-
-      return r
-    })
-  )
+export default function (listener: FlatfileListener) {
+  listener.use(ExcelExtractor())
 }
