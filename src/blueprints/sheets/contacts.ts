1727c69350ddd51ae338e46a0271324cd38b0e19
Carl Brugger <carl.brugger@flatfile.io>
2024-02-07 10:13:16 -0600

    Update playground base

--- src/blueprints/sheets/contacts.ts
+++ src/blueprints/sheets/contacts.ts
@@ -3,21 +3,43 @@ import { Flatfile } from '@flatfile/api'
 export const contactsSheet: Flatfile.SheetConfig = {
   name: 'Contacts',
   slug: 'contacts',
+  allowAdditionalFields: true,
   fields: [
     {
       key: 'firstName',
       type: 'string',
       label: 'First Name',
+      constraints: [{ type: 'required' }, { type: 'unique' }],
     },
     {
       key: 'lastName',
       type: 'string',
       label: 'Last Name',
+      // constraints: [
+      //   { type: 'external', validator: 'length', config: { max: 10 } },
+      // ],
     },
     {
       key: 'email',
       type: 'string',
       label: 'Email',
+      // constraints: [
+      //   {
+      //     type: 'external',
+      //     validator: 'email',
+      //     config: { emailRegex: '^[^s@]+@[^s@]+.[^s@]+$' },
+      //   },
+      // ],
+    },
+    {
+      key: 'dob',
+      type: 'date',
+      label: 'DOB',
+    },
+    {
+      key: 'age',
+      type: 'number',
+      label: 'Age',
     },
   ],
   actions: [
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,88 @@
 import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
-import { FlatfileRecord, recordHook } from '@flatfile/plugin-record-hook'
 
 export default async function (listener: FlatfileListener) {
-  listener.use(
-    recordHook('contacts', (r: FlatfileRecord, event: FlatfileEvent) => {
-      const email = r.get('email') as string
-      if (!email) {
-        console.log('Email is required')
-        r.addError('email', 'Email is required')
-      }
+  listener.on('**', (event: FlatfileEvent) => {
+    console.log(event.target)
+  })
 
-      const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-      if (email !== null && !validEmailAddress.test(email)) {
-        console.log('Invalid email address')
-        r.addError('email', 'Invalid email address')
-      }
+  // Plugin example:
+  // listener.use(
+  //   bulkRecordHook(
+  //     'oneHundred',
+  //     async (records: FlatfileRecord[]) => {
+  //       for (const record of records) {
+  //         const email = record.get('email') as string
+  //         const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  //         if (!validEmailAddress.test(email)) {
+  //           record.addError('email', 'Error: Invalid email address')
+  //         }
+  //       }
+  //       return records
+  //     },
+  //     { debug: true }
+  //   )
+  // )
 
-      return r
-    })
-  )
+  // Namespace example:
+  // listener.namespace(
+  //   ['space:getting-started'],
+  //   configureSpace(
+  //     {
+  //       workbooks: [
+  //         {
+  //           name: 'Getting Started',
+  //           sheets: [contactsSheet, fieldTypesSheet],
+  //           // settings: {
+  //           //   trackChanges: true,
+  //           // },
+  //           actions: [
+  //             {
+  //               operation: 'submitActionFg',
+  //               mode: 'foreground',
+  //               label: 'Submit data',
+  //               type: 'string',
+  //               description: 'Submit this data to a webhook.',
+  //               primary: true,
+  //             },
+  //             {
+  //               operation: 'downloadWorkbook',
+  //               mode: 'foreground',
+  //               label: 'Download Workbook',
+  //               description: 'Downloads Excel Workbook of Data',
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //       space: {
+  //         metadata: {
+  //           theme: {
+  //             root: {
+  //               primaryColor: 'black',
+  //             },
+  //             sidebar: {
+  //               logo: 'https://images.ctfassets.net/hjneo4qi4goj/33l3kWmPd9vgl1WH3m9Jsq/13861635730a1b8af383a8be8932f1d6/flatfile-black.svg',
+  //             },
+  //           },
+  //         },
+  //       },
+  //       documents: [
+  //         {
+  //           title: 'Welcome',
+  //           body: `<div>
+  //           <h1 style="margin-bottom: 36px;">Welcome!</h1>
+  //           <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
+  //           <h2 style="margin-bottom: 0px;">1. Step One</h2>
+  //           <p style="margin-top: 0px; margin-bottom: 8px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+  //           <h2 style="margin-bottom: 0px;">2. Step Two</h2>
+  //           <p style="margin-top: 0px; margin-bottom: 8px;">Consectetur libero id faucibus nisl tincidunt eget. Pellentesque elit eget gravida cum sociis natoque penatibus et. Tempor orci eu lobortis elementum nibh.</p>
+  //           </div>`,
+  //         },
+  //       ],
+  //     },
+  //     async (event, workbookIds, tick) => {
+  //       const { spaceId } = event.context
+  //       console.log('Space configured', { spaceId, workbookIds })
+  //     }
+  //   )
+  // )
 }
