6920b347ffb90a5806ce7b8c9d6e2d0f6fc86e3a
Carl Brugger <carl.brugger@flatfile.io>
2024-04-03 12:31:17 -0500

    webhook egress example

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,70 @@
+import type { Flatfile } from '@flatfile/api'
+import api from '@flatfile/api'
 import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
+import type { RejectionResponse } from '@flatfile/util-response-rejection'
+import { responseRejectionHandler } from '@flatfile/util-response-rejection'
 
-export default async function (listener: FlatfileListener) {
-  listener.on('**', (event: FlatfileEvent) => {
-    console.log(event.target)
-  })
+export default function (listener: FlatfileListener) {
+  listener.on(
+    'job:ready',
+    { job: 'workbook:submitActionFg' },
+    async (event: FlatfileEvent) => {
+      const { jobId } = event.context
 
-  // Plugin example:
-  // listener.use(
-  //   bulkRecordHook(
-  //     'oneHundred',
-  //     async (records: FlatfileRecord[]) => {
-  //       for (const record of records) {
-  //         const email = record.get('email') as string
-  //         const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  //         if (!validEmailAddress.test(email)) {
-  //           record.addError('email', 'Error: Invalid email address')
-  //         }
-  //       }
-  //       return records
-  //     },
-  //     { debug: true }
-  //   )
-  // )
+      await api.jobs.ack(jobId, {
+        info: 'Accepted',
+        progress: 1,
+      })
 
-  // Namespace example:
-  // listener.namespace(
-  //   ['space:getting-started'],
-  //   configureSpace(
-  //     {
-  //       workbooks: [
-  //         {
-  //           name: 'Getting Started',
-  //           sheets: [contactsSheet, fieldTypesSheet],
-  //           // settings: {
-  //           //   trackChanges: true,
-  //           // },
-  //           actions: [
-  //             {
-  //               operation: 'submitActionFg',
-  //               mode: 'foreground',
-  //               label: 'Submit data',
-  //               type: 'string',
-  //               description: 'Submit this data to a webhook.',
-  //               primary: true,
-  //             },
-  //             {
-  //               operation: 'downloadWorkbook',
-  //               mode: 'foreground',
-  //               label: 'Download Workbook',
-  //               description: 'Downloads Excel Workbook of Data',
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //       space: {
-  //         metadata: {
-  //           theme: {
-  //             root: {
-  //               primaryColor: 'black',
-  //             },
-  //             sidebar: {
-  //               logo: 'https://images.ctfassets.net/hjneo4qi4goj/33l3kWmPd9vgl1WH3m9Jsq/13861635730a1b8af383a8be8932f1d6/flatfile-black.svg',
-  //             },
-  //           },
-  //         },
-  //       },
-  //       documents: [
-  //         {
-  //           title: 'Welcome',
-  //           body: `<div>
-  //           <h1 style="margin-bottom: 36px;">Welcome!</h1>
-  //           <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
-  //           <h2 style="margin-bottom: 0px;">1. Step One</h2>
-  //           <p style="margin-top: 0px; margin-bottom: 8px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-  //           <h2 style="margin-bottom: 0px;">2. Step Two</h2>
-  //           <p style="margin-top: 0px; margin-bottom: 8px;">Consectetur libero id faucibus nisl tincidunt eget. Pellentesque elit eget gravida cum sociis natoque penatibus et. Tempor orci eu lobortis elementum nibh.</p>
-  //           </div>`,
-  //         },
-  //       ],
-  //     },
-  //     async (event, workbookIds, tick) => {
-  //       const { spaceId } = event.context
-  //       console.log('Space configured', { spaceId, workbookIds })
-  //     }
-  //   )
-  // )
+      // Default outcome for successful submission
+      let outcome: Flatfile.JobCompleteDetails = {
+        outcome: {
+          message: `Data was successfully submitted with no rejections.`,
+        },
+      }
+
+      try {
+        const data = [] // TODO: implement your own logic here for retrieving the records and formatting them for the webhook
+        const response = await fetch('https://webhook.site/...', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        const responseData = await response.json()
+
+        const rejections: RejectionResponse = responseData.rejections
+        if (rejections) {
+          // Update outcome for rejections
+          outcome = await responseRejectionHandler(rejections)
+        }
+
+        await api.jobs.complete(
+          jobId,
+          outcome ?? {
+            outcome: {
+              message: 'Job complete',
+            },
+          }
+        )
+      } catch (error) {
+        await api.jobs.fail(
+          jobId,
+          outcome ?? {
+            info: String(error.message),
+            outcome: {
+              acknowledge: true,
+              message: String(error.message),
+            },
+          }
+        )
+      }
+    }
+  )
 }
