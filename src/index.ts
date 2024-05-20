405c04487a5cfc5ffca89d009f3aa97bae3d2cfe
Carl Brugger <carl.brugger@flatfile.io>
2024-05-20 14:29:41 -0500

    Dep update & activate configureSpace plugin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import type { Flatfile } from '@flatfile/api'
 import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
+
+import { configureSpace } from '@flatfile/plugin-space-configure'
+import { contactsSheet } from './blueprints/sheets/contacts'
 
 export default async function (listener: FlatfileListener) {
   listener.on('**', (event: FlatfileEvent) => {
@@ -24,65 +28,65 @@ export default async function (listener: FlatfileListener) {
   // )
 
   // Configure space example:
-  // listener.namespace(
-  //   ['space:getting-started'],
-  //   configureSpace(
-  //     {
-  //       workbooks: [
-  //         {
-  //           name: 'Getting Started',
-  //           sheets: [contactsSheet, fieldTypesSheet],
-  //           // settings: {
-  //           //   trackChanges: true,
-  //           // },
-  //           actions: [
-  //             {
-  //               operation: 'submitActionFg',
-  //               mode: 'foreground',
-  //               label: 'Submit data',
-  //               type: 'string',
-  //               description: 'Submit this data to a webhook.',
-  //               primary: true,
-  //             },
-  //             {
-  //               operation: 'downloadWorkbook',
-  //               mode: 'foreground',
-  //               label: 'Download Workbook',
-  //               description: 'Downloads Excel Workbook of Data',
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //       space: {
-  //         metadata: {
-  //           theme: {
-  //             root: {
-  //               primaryColor: 'black',
-  //             },
-  //             sidebar: {
-  //               logo: 'https://images.ctfassets.net/hjneo4qi4goj/33l3kWmPd9vgl1WH3m9Jsq/13861635730a1b8af383a8be8932f1d6/flatfile-black.svg',
-  //             },
-  //           },
-  //         },
-  //       },
-  //       documents: [
-  //         {
-  //           title: 'Welcome',
-  //           body: `<div>
-  //           <h1 style="margin-bottom: 36px;">Welcome!</h1>
-  //           <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
-  //           <h2 style="margin-bottom: 0px;">1. Step One</h2>
-  //           <p style="margin-top: 0px; margin-bottom: 8px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-  //           <h2 style="margin-bottom: 0px;">2. Step Two</h2>
-  //           <p style="margin-top: 0px; margin-bottom: 8px;">Consectetur libero id faucibus nisl tincidunt eget. Pellentesque elit eget gravida cum sociis natoque penatibus et. Tempor orci eu lobortis elementum nibh.</p>
-  //           </div>`,
-  //         },
-  //       ],
-  //     },
-  //     async (event, workbookIds, tick) => {
-  //       const { spaceId } = event.context
-  //       console.log('Space configured', { spaceId, workbookIds })
-  //     }
-  //   )
-  // )
+  listener.namespace(
+    ['space:getting-started'],
+    configureSpace(
+      {
+        workbooks: [
+          {
+            name: 'Getting Started',
+            sheets: [contactsSheet],
+            // settings: {
+            //   trackChanges: true,
+            // },
+            actions: [
+              {
+                operation: 'submitActionFg',
+                mode: 'foreground',
+                label: 'Submit data',
+                type: 'string',
+                description: 'Submit this data to a webhook.',
+                primary: true,
+              },
+              {
+                operation: 'downloadWorkbook',
+                mode: 'foreground',
+                label: 'Download Workbook',
+                description: 'Downloads Excel Workbook of Data',
+              },
+            ],
+          },
+        ],
+        space: {
+          metadata: {
+            theme: {
+              root: {
+                primaryColor: 'black',
+              },
+              sidebar: {
+                logo: 'https://images.ctfassets.net/hjneo4qi4goj/33l3kWmPd9vgl1WH3m9Jsq/13861635730a1b8af383a8be8932f1d6/flatfile-black.svg',
+              },
+            },
+          },
+        },
+        documents: [
+          {
+            title: 'Welcome',
+            body: `<div>
+            <h1 style="margin-bottom: 36px;">Welcome!</h1>
+            <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
+            <h2 style="margin-bottom: 0px;">1. Step One</h2>
+            <p style="margin-top: 0px; margin-bottom: 8px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <h2 style="margin-bottom: 0px;">2. Step Two</h2>
+            <p style="margin-top: 0px; margin-bottom: 8px;">Consectetur libero id faucibus nisl tincidunt eget. Pellentesque elit eget gravida cum sociis natoque penatibus et. Tempor orci eu lobortis elementum nibh.</p>
+            </div>`,
+          },
+        ],
+      },
+      async (event, workbookIds, tick) => {
+        const { spaceId } = event.context
+        console.log('Space configured', { spaceId, workbookIds })
+      }
+    )
+  )
 }
