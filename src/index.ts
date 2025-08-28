1d51192118a805084cf54a180c29d6ea94ae0e89
Carl Brugger <carl.brugger@flatfile.io>
2025-08-28 11:12:38 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,49 @@
-import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
-
-import { bulkRecordHook, type FlatfileRecord } from '@flatfile/plugin-record-hook'
-import { configureSpace } from '@flatfile/plugin-space-configure'
-import { XMLExtractor } from '@flatfile/plugin-xml-extractor'
-import { contactsSheet } from './blueprints/sheets/contacts'
+import api, { type Flatfile } from '@flatfile/api'
+import type { FlatfileListener } from '@flatfile/listener'
+import { exportWorkbookPlugin } from '@flatfile/plugin-export-workbook'
+import '@flatfile/http-logger/init'
 
 export default async function (listener: FlatfileListener) {
-  listener.on('**', (event: FlatfileEvent) => {
-    console.log(event.target)
+  // First: Cache the sheets to avoid excessive API calls
+  listener.on('job:ready', { job: 'workbook:downloadWorkbook' }, async (e) => {
+    await e.cache.init('sheets-schema', async () => {
+      const { data } = await api.sheets.list({
+        workbookId: e.context.workbookId,
+      })
+      return data
+    })
   })
-
-  listener.use(XMLExtractor())
-
-  // Record hook example:
+  // Then: Export workbook
   listener.use(
-    bulkRecordHook(
-      'contacts',
-      async (records: FlatfileRecord[]) => {
-        for (const record of records) {
-          const email = record.get('email') as string
-          const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-          if (!validEmailAddress.test(email)) {
-            record.addError('email', 'Error: Invalid email address')
+    exportWorkbookPlugin({
+      autoDownload: true,
+      columnNameTransformer: async (columnName, sheetSlug, e) => {
+        try {
+          // Get all sheets for the workbook
+          const sheets = await e?.cache.get<Flatfile.Sheet[]>('sheets-schema')
+          if (!sheets) {
+            return columnName
           }
-        }
-        return records
-      },
-      { debug: true },
-    ),
-  )
 
-  // Configure space example:
-  listener.namespace(
-    ['space:getting-started'],
-    configureSpace(
-      {
-        workbooks: [
-          {
-            name: 'Getting Started',
-            sheets: [contactsSheet],
-            // settings: {
-            //   trackChanges: true,
-            // },
-            actions: [
-              {
-                operation: 'submitActionFg',
-                mode: 'foreground',
-                label: 'Submit data',
-                type: 'string',
-                description: 'Submit this data to a webhook.',
-                primary: true,
-              },
-            ],
-          },
-        ],
-        space: {
-          metadata: {
-            theme: {
-              root: {
-                primaryColor: 'black',
-              },
-              sidebar: {
-                logo: 'https://images.ctfassets.net/hjneo4qi4goj/33l3kWmPd9vgl1WH3m9Jsq/13861635730a1b8af383a8be8932f1d6/flatfile-black.svg',
-              },
-            },
-          },
-        },
-        documents: [
-          {
-            title: 'Welcome',
-            body: `<div>
-            <h1 style="margin-bottom: 36px;">Welcome!</h1>
-            <h2 style="margin-top: 0px; margin-bottom: 12px;">To get started, follow these steps:</h2>
-            <h2 style="margin-bottom: 0px;">1. Step One</h2>
-            <p style="margin-top: 0px; margin-bottom: 8px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <h2 style="margin-bottom: 0px;">2. Step Two</h2>
-            <p style="margin-top: 0px; margin-bottom: 8px;">Consectetur libero id faucibus nisl tincidunt eget. Pellentesque elit eget gravida cum sociis natoque penatibus et. Tempor orci eu lobortis elementum nibh.</p>
-            </div>`,
-          },
-        ],
+          const sheet = sheets.find((s) => s.slug === sheetSlug)
+          if (!sheet) {
+            return columnName
+          }
+
+          if (sheet?.config?.fields) {
+            // Find the field with the matching key
+            const field = sheet.config.fields.find((f) => f.key === columnName)
+
+            // Return the label if found, otherwise return the original column name
+            return field?.label || columnName
+          }
+
+          return columnName
+        } catch (error) {
+          console.error('Error accessing field configuration:', error)
+          return columnName
+        }
       },
-      async (event, workbookIds, _tick) => {
-        const { spaceId } = event.context
-        console.log('Space configured', { spaceId, workbookIds })
-      },
-    ),
+    }),
   )
 }
